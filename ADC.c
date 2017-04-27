e6f936863aebe911a0b1340778d763ef72cc3779
arvinbhatti <arvinbhatti@gmail.com>
2017-04-26 21:49:49 -0500

    Add files via upload
    movement of helicopter done

--- ADC.c
+++ ADC.c
@@ -1,2 +1,57 @@
-// put implementations for functions, explain how it works
-// put your names here, date
+// ADC.c
+// Runs on LM4F120/TM4C123
+// Provide functions that initialize ADC0
+// Last Modified: 3/6/2015 
+// Student names: Arvin Bhatti and Josh Thias
+// Last modification date: 4/11/17
+
+#include <stdint.h>
+#include "tm4c123gh6pm.h"
+
+	
+
+// ADC initialization function 
+// Input: none
+// Output: none
+void ADC_Init(void){ volatile int delay;
+	
+	//need to fix the delay varialbe
+
+	SYSCTL_RCGCGPIO_R |= 0x10;  	// 1) activate clock for Port E 
+  while((SYSCTL_PRGPIO_R&0x10) == 0){};
+  GPIO_PORTE_DIR_R &= ~0x10;      // 2) make PE4 input
+  GPIO_PORTE_AFSEL_R |= 0x10;     // 3) enable alternate fun on PE4
+  GPIO_PORTE_DEN_R &= ~0x10;      // 4) disable digital I/O on PE4
+  GPIO_PORTE_AMSEL_R |= 0x10;     // 5) enable analog fun on PE4
+  SYSCTL_RCGCADC_R |= 0x01;       // 6) activate ADC0 
+  delay = SYSCTL_RCGCADC_R;       // extra time to stabilize
+  delay = SYSCTL_RCGCADC_R;       // extra time to stabilize
+  delay = SYSCTL_RCGCADC_R;       // extra time to stabilize
+  delay = SYSCTL_RCGCADC_R;
+  ADC0_PC_R = 0x01;               // 7) configure for 125K 
+  ADC0_SSPRI_R = 0x0123;          // 8) Seq 3 is highest priority
+  ADC0_ACTSS_R &= ~0x0008;        // 9) disable sample sequencer 3
+  ADC0_EMUX_R &= ~0xF000;         // 10) seq3 is software trigger
+  ADC0_SSMUX3_R = (ADC0_SSMUX3_R&0xFFFFFFF0)+9;  // 11) Ain9 (PE4)
+  ADC0_SSCTL3_R = 0x0006;         // 12) no TS0 D0, yes IE0 END0
+  ADC0_IM_R &= ~0x0008;           // 13) disable SS3 interrupts
+  ADC0_ACTSS_R |= 0x0008;         // 14) enable sample sequencer 3
+
+
+
+}
+
+//------------ADC_In------------
+// Busy-wait Analog to digital conversion
+// Input: none
+// Output: 12-bit result of ADC conversion
+uint32_t ADC_In(void){  
+	uint32_t result;
+	ADC0_PSSI_R = 0x0008;						//initiate SS3
+	while((ADC0_RIS_R&0x08)==0){};	//wait for conversion done
+	result = ADC0_SSFIFO3_R&0xFFF;	//read 12-bit result
+	ADC0_ISC_R = 0x0008;						//acknowledge completion
+  return result; 									// store back result
+}
+
+
--- Print.s
+++ Print.s
@@ -17,7 +17,12 @@
     AREA    |.text|, CODE, READONLY, ALIGN=2
     THUMB
 
-  
+LV EQU 0    ;local variable for LCD_OutDec, used for link register
+	
+C1 EQU 0    ;first character
+C2 EQU 4    ;second character
+C3 EQU 8    ;third character
+C4 EQU 12   ;fourth character
 
 ;-----------------------LCD_OutDec-----------------------
 ; Output a 32-bit number in unsigned decimal format
@@ -25,8 +30,22 @@
 ; Output: none
 ; Invariables: This function must not permanently modify registers R4 to R11
 LCD_OutDec
-
-
+	  SUB SP, #8          ; allocation for local variables
+	  STR LR, [SP, #LV]
+	  CMP R0, #10         ;checks if the character is less than 10, if so ends
+	  BLO OEND
+	  MOV R2, #10         ;make r2 number 10
+	  UDIV R3, R0, R2     ;r3=r0/r2, or r3=input/10
+	  MUL R1, R3, R2      ;r1=r3*r2, or r1=(input/10)*10
+	  SUB R1, R0, R1      ;r1=remainder
+	  PUSH {R1, R11}      ;pushes remainder to stack
+	  MOVS R0, R3         ;r0=input/10
+	  BL LCD_OutDec
+	  POP {R0, R11}       
+OEND  ADD R0, R0, #0x30
+      BL ST7735_OutChar   ;outputs remainder to LCD
+	  LDR LR, [SP, #LV]
+	  ADD SP, #8
       BX  LR
 ;* * * * * * * * End of LCD_OutDec * * * * * * * *
 
@@ -43,9 +62,60 @@ LCD_OutDec
 ;       R0>9999, then output "*.*** "
 ; Invariables: This function must not permanently modify registers R4 to R11
 LCD_OutFix
-
-     BX   LR
- 
+	  PUSH {R4, LR}
+	  SUB SP, #16			; Allocation for local variables
+	  MOV R4, #10000        ;check if r0 is 10000, if so output *.***
+	  CMP R0, R4
+	  BHS STAR
+	                      ;first character
+	  MOV R2, #10         ;make r2 number 10
+	  UDIV R3, R0, R2     ;r3=r0/r2, or r3=input/10
+	  MUL R1, R3, R2      ;r1=r3*r2, or r1=(input/10)*10
+	  SUB R1, R0, R1      ;r1=remainder
+	  ADD R1, #0x30
+	  STR R1, [SP, #C1]
+	  MOVS R0, R3         ;r0=input/10
+	                      ;second character
+	  UDIV R3, R0, R2     ;r3=r0/r2, or r3=input/10
+	  MUL R1, R3, R2      ;r1=r3*r2, or r1=(input/10)*10
+	  SUB R1, R0, R1      ;r1=remainder
+	  ADD R1, #0x30
+	  STR R1, [SP, #C2]
+	  MOVS R0, R3         ;r0=input/10
+	                      ;third character
+	  UDIV R3, R0, R2     ;r3=r0/r2, or r3=input/10
+	  MUL R1, R3, R2      ;r1=r3*r2, or r1=(input/10)*10
+	  SUB R1, R0, R1      ;r1=remainder
+	  ADD R1, #0x30
+	  STR R1, [SP, #C3]
+	  MOVS R0, R3         ;r0=input/10
+	                      ;fourth character
+	  UDIV R3, R0, R2     ;r3=r0/r2, or r3=input/10
+	  MUL R1, R3, R2      ;r1=r3*r2, or r1=(input/10)*10
+	  SUB R1, R0, R1      ;r1=remainder
+	  ADD R1, #0x30
+	  STR R1, [SP, #C4]
+	  
+	  B OUT
+STAR	
+	  MOV R0, #0x2A       ;puts stars in all the characters
+	  STR R0, [SP, #C1]
+	  STR R0, [SP, #C2]
+	  STR R0, [SP, #C3]
+	  STR R0, [SP, #C4]
+	
+OUT	 LDR R0, [SP, #C4]
+     BL ST7735_OutChar
+	 MOV R0, #0x2E       ;puts a decimal before the next three characters
+	 BL ST7735_OutChar
+	 LDR R0, [SP, #C3]
+     BL ST7735_OutChar
+	 LDR R0, [SP, #C2]
+     BL ST7735_OutChar
+	 LDR R0, [SP, #C1]
+	 BL ST7735_OutChar
+	 ADD SP, #16
+	 POP {R4, PC}
      ALIGN
 ;* * * * * * * * End of LCD_OutFix * * * * * * * *
 
